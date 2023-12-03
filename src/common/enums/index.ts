9c2fa77d0570f469ba2efcbe51a9e02230ed64b4
Sardor <76036267+Bakhromov-02@users.noreply.github.com>
2023-12-03 19:09:03 +0500

    import fixes

--- src/common/enums/index.ts
+++ src/common/enums/index.ts
@@ -1,5 +1,4 @@
 export * from './roles.enum';
 export * from './countries.enum';
 export * from './languages.enum';
-export * from './salary-payment-type.enum';
 export * from './postgres-error-code.enum';
